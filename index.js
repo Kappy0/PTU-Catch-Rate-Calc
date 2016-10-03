ecb05e47bf4778bf25f915c2cda0a67ac038970b
Kappy0 <ryanklaproth@gmail.com>
2016-10-02 20:47:41 -0500

    Variable Assignment
    All variables are now assigned their proper value.

--- index.js
+++ index.js
@@ -15,10 +15,55 @@ function calc()
 	var numVolatile, numPersistent;
 	var stuck, slow;
 	
+	//Assign variables
+	level = parseInt(document.calcForm.level.value);
+	maxHP = parseInt(document.calcForm.maxhp.value);
+	curHP = parseInt(document.calcForm.curhp.value);
+	evo = parseInt(document.calcForm.evo.value);
+	shiny = document.calcForm.shiny.checked;
+	legend = document.calcForm.legend.checked;
+	numInjuries = parseInt(document.calcForm.injury.value);
+	numVolatile = parseInt(document.calcForm.stvolatile.value);
+	numPersistent = parseInt(document.calcForm.stpersistent.value);
+	stuck = document.calcForm.stuck.checked;
+	slow = document.calcForm.slow.checked;
 	
+	document.getElementById("result").innerHTML = "Level: " + level + "\n" +
+	"Max HP: " + maxHP + "\n" +
+	"Current HP: " + curHP + "\n" +
+	"Evolutions Remaining: " + evo + "\n" +
+	"Shiny? " + shiny + "\n" +
+	"Legendary? " + legend + "\n" +
+	"Number of Injuries: " + numInjuries + "\n" +
+	"Number of Volatile Status Effects: " + numVolatile + "\n" +
+	"Number of Persistent Status Effects: " + numPersistent + "\n" +
+	"Stuck? " + stuck + "\n" +
+	"Slowed? " + slow;
 	
-	document.getElementById("catchrate").innerHTML = "TEXT WILL GO HERE!";
+	/*if(checkIfNaN(level, maxHP, curHP, evo, numInjuries, numVolatile, numPersistent) == true)
+	{
+		document.getElementById("result").innerHTML = "TEXT WILL GO HERE!";
+	}
+	else
+	{
+		document.getElementById("result").innerHTML = "ERROR! SOME INPUTS NOT ENTERED.";
+	}*/
 }
+
+/*function checkIfNaN(var c_level, var c_maxHP, var c_curHP, var c_evo, var c_numInjuries, var c_numVolatile, var c_numPersistent)
+{
+	var bool = true;
+	
+	if(isNaN(c_level))         {bool = false;}
+	if(isNaN(c_maxHP))         {bool = false;}
+	if(isNaN(c_curHP))         {bool = false;}
+	if(isNaN(c_evo))           {bool = false;}
+	if(isNaN(c_numInjuries))   {bool = false;}
+	if(isNaN(c_numVolatile))   {bool = false;}
+	if(isNaN(c_numPersistent)) {bool = false;}
+	
+	return bool;
+}*/
 
 function addLoadEvent(funct)
 {
